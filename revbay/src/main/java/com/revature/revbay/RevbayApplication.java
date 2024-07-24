ce22b64942399163358ce3330d6dc10a9b5e3953
JacobMRyan12 <jacob.martin.ryan.1.2@gmail.com>
2024-07-24 15:05:16 -0500

    Setting up Spring Stuff

---
+++ revbay/src/main/java/com/revature/revbay/RevbayApplication.java
@@ -0,0 +1,13 @@
+package com.revature.revbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevbayApplication.class, args);
+	}
+
+}
---
+++ revbay/src/test/java/com/revature/revbay/RevbayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.revbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

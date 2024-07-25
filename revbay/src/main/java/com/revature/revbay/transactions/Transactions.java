8c924bfdd767f45a81f5f83ae247d28ab241e177
JacobMRyan12 <jacob.martin.ryan.1.2@gmail.com>
2024-07-25 16:21:46 -0500

    Added maven dependancy and Continuing to work on Transaction

--- revbay/src/main/java/com/revature/revbay/transactions/Transactions.java
+++ revbay/src/main/java/com/revature/revbay/transactions/Transactions.java
@@ -1,17 +1,37 @@
 package com.revature.revbay.transactions;
 
+import jakarta.persistence.*;
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.sql.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+//JPA
+@Entity
+@Table(name = "transactions")
 public class Transactions {
+    @Id
+    @SequenceGenerator(name = "TransactionNumberGen", initialValue=1)
+    @GeneratedValue(generator = "TransactionNumberGen")
     //Declaring Variables
     private int transactionID;
+    @ManyToOne
+    @JoinColumn(name="user_id")
     private int sellerID;
+    @ManyToOne
+    @JoinColumn(name="user_id")
     private int buyerID;
     private BigDecimal totalPrice;
     private String destination;
     //Might need to be changed to a different Date Type
     private Date currentTime;
+
 
     public Transactions(){}
 

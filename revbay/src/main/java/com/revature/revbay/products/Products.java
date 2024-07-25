fc61b2287fc6575d7a526de0783b366f52c46004
Asaf Ahmed <asafahmed@Asafs-Mac-mini.local>
2024-07-25 14:30:18 -0500

    implemented basic products feature logic and jpa

--- revbay/src/main/java/com/revature/revbay/products/Products.java
+++ revbay/src/main/java/com/revature/revbay/products/Products.java
@@ -1,4 +1,30 @@
 package com.revature.revbay.products;
 
+import com.revature.revbay.user.User;
+import com.revature.revbay.util.enums.Category;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Products")
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int productId;
+    @Column(unique = true,nullable = false)
+    private String name;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(columnDefinition = "integer check (quantity>-1)")
+    private int quantity;
+    @Column(columnDefinition = "double precision (price>0)")
+    private Double price;
 }

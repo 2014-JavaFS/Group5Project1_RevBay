50a143331a197c4069bbf509673427fe805c68ff
Asaf Ahmed <asafahmed@Asafs-Mac-mini.local>
2024-07-29 16:51:30 -0500

    temp

--- revbay/src/main/java/com/revature/revbay/products/ProductsRepository.java
+++ revbay/src/main/java/com/revature/revbay/products/ProductsRepository.java
@@ -2,5 +2,9 @@ package com.revature.revbay.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductsRepository extends JpaRepository<Products,Integer> {
+    Optional<List<Products>> findByUserId(int userId);
 }

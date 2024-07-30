69b8b28b64f591559969e17d07a972a0e3b30510
Asaf Ahmed <asafahmed@Asafs-Mac-mini.local>
2024-07-30 15:21:29 -0500

    typo fix

--- revbay/src/main/java/com/revature/revbay/products/ProductsRepository.java
+++ revbay/src/main/java/com/revature/revbay/products/ProductsRepository.java
@@ -2,10 +2,11 @@ package com.revature.revbay.products;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface ProductsRepository extends JpaRepository<Products,Integer> {
     @Query("FROM Products  p WHERE p.user.userId = :userId")
     Optional<List<Products>> findProductsByUserId(int userId);
